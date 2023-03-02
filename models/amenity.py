96695d1ce16e75d916d03364f65e0a7ae1f90dc1
Segun <ojoolusegun17@gmail.com>
2023-03-02 03:06:48 -0800

    correct model amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,5 +5,17 @@ from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Class representing a Amenity."""
+    """Amenity class inherits from BaseModel
+    Attribute:
+        name (str): Public class attribute for Amenity's name
+    """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method for Amenity class
+        Attributes:
+            args (list): The list with arguments
+            kwargs (dict): A dictionary with arguments
+        """
+        super().__init__(*args, **kwargs)
+
