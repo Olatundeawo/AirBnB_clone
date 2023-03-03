4f451a257e9ec204e10fa62ff3f4617e6a69a310
Segun <ojoolusegun17@gmail.com>
2023-03-02 20:42:37 -0800

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-"""Module for Amenity class."""
+
+"""
+    Defines a class Amenity.
+"""
 
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Amenity class inherits from BaseModel
-    Attribute:
-        name (str): Public class attribute for Amenity's name
-    """
+    """Represent a Amenity."""
+
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """init method for Amenity class
-        Attributes:
-            args (list): The list with arguments
-            kwargs (dict): A dictionary with arguments
-        """
-        super().__init__(*args, **kwargs)
-
